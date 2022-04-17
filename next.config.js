f29cc5c3ea19d1c19e629da9431a917505644890
NDHLOVES <Es2020*$>
2022-04-17 15:07:11 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        //"MONGODB_URL": "mongodb://localhost:27017/nextshop",
+        "MONGODB_URL": "mongodb+srv://openchoice:p9uRjVD01iesLDB6@cluster0.dhkbd.mongodb.net/next_cakeshop?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "k&T(cHK3U[a4N(*n9YG&",
+        "REFRESH_TOKEN_SECRET": "<qdN:dNC6q/9?S,S{wTc",
+        "PAYPAL_CLIENT_ID": "AZW_kTQUzaZxRe3TpktTryTlSusg04NtOvZbA3xDclbuMK6iOF74Aoqias8bSpdeTlSohKBZZYMHTfmk",
+        "CLOUD_UPDATE_PRESET": "nextjs_store",
+        "CLOUD_NAME": "taizyandhlovu",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/taizyandhlovu/image/upload"
+    }
 }
-
-module.exports = nextConfig
